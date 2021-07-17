b897dd8c9c3bf794f31477512a3f02da0a00e8ef
Koidn <thomas.kaltenleitner@outlook.com>
2021-07-17 17:18:14 +0200

    experimenting with dates

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,16 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Main {
+    static String date = "1.9.21";
 
     public static void main(String[] args) {
 	ReadFiles test = new ReadFiles();
 	test.printOut();
+
+	DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d.M.u");
+        LocalDate testdate = LocalDate.parse(date,formatter);
+
+	System.out.println(testdate.getMonth());
     }
 }
