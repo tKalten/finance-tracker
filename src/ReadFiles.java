d658e35c556e6cd5674b72f68a719ccce09cb155
Koidn <thomas.kaltenleitner@outlook.com>
2021-07-18 03:04:03 +0200

    changed folder structure

--- src/ReadFiles.java
+++ src/ReadFiles.java
@@ -2,7 +2,7 @@ import java.io.File;
 import java.util.Scanner;
 
 public class ReadFiles {
-    static File file = new File("C:\\Users\\thoma\\OneDrive - Technische Universität Graz\\privat\\java\\20210717finance_tracker\\finance_lists\\16.07.txt");
+    static File file = new File("C:\\Users\\thoma\\OneDrive - Technische Universität Graz\\privat\\java\\20210717finance_tracker\\test_lists\\16.07.txt");
     static Scanner sc;
     static String line;
 
